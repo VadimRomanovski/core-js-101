adaaecaf47ea5e0575cbf4ad3edf36fbe31c0cf7
VadimRomanovski <49916251+VadimRomanovski@users.noreply.github.com>
2020-05-17 22:21:21 +0300

    feat: resolve tasks

--- src/04-date-tasks.js
+++ src/04-date-tasks.js
@@ -19,8 +19,10 @@
  *    'Tue, 26 Jan 2016 13:48:02 GMT' => Date()
  *    'Sun, 17 May 1998 03:00:00 GMT+01' => Date()
  */
-function parseDataFromRfc2822(/* value */) {
-  throw new Error('Not implemented');
+function parseDataFromRfc2822(value) {
+  const d = new Date(value);
+  return Date.UTC(d.getUTCFullYear(), d.getUTCMonth(), d.getUTCDate(),
+    d.getUTCHours(), d.getUTCMinutes(), d.getUTCSeconds());
 }
 
 /**
@@ -34,8 +36,8 @@ function parseDataFromRfc2822(/* value */) {
  *    '2016-01-19T16:07:37+00:00'    => Date()
  *    '2016-01-19T08:07:37Z' => Date()
  */
-function parseDataFromIso8601(/* value */) {
-  throw new Error('Not implemented');
+function parseDataFromIso8601(value) {
+  return new Date(value);
 }
 
 
@@ -53,8 +55,10 @@ function parseDataFromIso8601(/* value */) {
  *    Date(2012,1,1)    => true
  *    Date(2015,1,1)    => false
  */
-function isLeapYear(/* date */) {
-  throw new Error('Not implemented');
+function isLeapYear(date) {
+  const y = date.getFullYear();
+  if ((y % 400 === 0) || ((y % 4 === 0) && (y % 100 !== 0))) return true;
+  return false;
 }
 
 
@@ -73,8 +77,8 @@ function isLeapYear(/* date */) {
  *    Date(2000,1,1,10,0,0),  Date(2000,1,1,10,0,0,250)     => "00:00:00.250"
  *    Date(2000,1,1,10,0,0),  Date(2000,1,1,15,20,10,453)   => "05:20:10.453"
  */
-function timeSpanToString(/* startDate, endDate */) {
-  throw new Error('Not implemented');
+function timeSpanToString(startDate, endDate) {
+  return (new Date(endDate - startDate)).toISOString().slice(11, -1);
 }
 
 
@@ -82,8 +86,6 @@ function timeSpanToString(/* startDate, endDate */) {
  * Returns the angle (in radians) between the hands of an analog clock
  * for the specified Greenwich time.
  * If you have problem with solution please read: https://en.wikipedia.org/wiki/Clock_angle_problem
- *
- * SMALL TIP: convert to radians just once, before return in order to not lost precision
  *
  * @param {date} date
  * @return {number}
@@ -94,8 +96,15 @@ function timeSpanToString(/* startDate, endDate */) {
  *    Date.UTC(2016,3,5,18, 0) => Math.PI
  *    Date.UTC(2016,3,5,21, 0) => Math.PI/2
  */
-function angleBetweenClockHands(/* date */) {
-  throw new Error('Not implemented');
+function angleBetweenClockHands(date) {
+  let h;
+  if (date.getUTCHours() > 12) {
+    h = date.getUTCHours() % 12;
+  } else {
+    h = date.getUTCHours();
+  }
+  const AngleResult = ((30 * h - 5.5 * date.getUTCMinutes()) * Math.PI) / 180;
+  return Math.abs((AngleResult > Math.PI) ? Math.PI * 2 - AngleResult : AngleResult);
 }
 
 
